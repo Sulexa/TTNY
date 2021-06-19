aeb992b0c506589f1eac80aa8f5356ca0a9953c1
sulexa <sulexa95@gmail.com>
2021-06-19 19:33:52 +0200

    Add working bot

---
+++ TwitchGames.TwitchChat/Models/TwitchAccessTokenModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TwitchGames.TwitchChat.Models
+{
+    public class TwitchAccessTokenModel
+    {
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; set; }
+
+        //public string RefreshToken { get; set; }
+        [JsonPropertyName("expires_in")]
+        public long ExpiresIn { get; set; }
+
+        //public List<object> Scope { get; set; }
+
+        [JsonPropertyName("token_type")]
+        public string TokenType { get; set; }
+    }
+}
---
+++ TwitchGames.TwitchChat/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+using TwitchGames.TwitchChat.Services;
+
+namespace TwitchGames.TwitchChat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{env}.json", true, true)
+                .AddEnvironmentVariables();
+
+            var config = builder.Build();
+
+            var bot = new Bot(config["Twitch:Username"], config["Twitch:AccessToken"]);
+
+            Console.ReadLine();
+        }
+    }
+}
